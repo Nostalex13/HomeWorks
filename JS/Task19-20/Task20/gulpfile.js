fd39d82f67428e8f5f95ecb52575e83b83c98aab
Nostalex13 <Nostalex523@gmail.com>
2017-03-24 16:18:41 +0200

    task19-20 final

---
+++ JS/Task19-20/Task20/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var concatCss = require('gulp-concat-css');
+var watch = require('gulp-watch');
+
+var paths = {
+  scripts: 'js/*.js'
+};
+
+gulp.task('scripts', function() {
+  return gulp.src(paths.scripts)
+    .pipe(concat('script.main.js'))
+    .pipe(gulp.dest('dist/scripts/'));
+});
+
+gulp.task('css', function() {
+   return gulp.src('css/*.css')
+     .pipe(concatCss("styles.main.css"))
+     .pipe(gulp.dest('dist/styles/'));
+});
+
+gulp.task('default', ['css', 'scripts'], function() {
+   gulp.watch(paths.scripts, ['scripts']);
+});
--- JS/Task19-20/Task20/js/script.js
+++ JS/Task19-20/Task20/js/script.js
@@ -1,3 +1,69 @@
 $(function() {
-   
+
+   // dataJson
+
+   /*          Skills         */
+
+   let skillsArray = [];
+
+   _.forEach(dataJson, function(obj) {
+       skillsArray += _.map(obj.skills, function(item) {
+          return item;
+       });
+
+       skillsArray += ',';
+   });
+
+   skillsArray = skillsArray.split(',');
+   skillsArray = _.compact(skillsArray);
+   skillsArray = _.uniq(skillsArray);
+
+   skillsArray = _.sortBy(skillsArray, function(item) {
+      return item.toLowerCase();
+   });
+
+   console.log('Skills array:', skillsArray);
+
+   /*          Names          */
+
+   let sortedByFriends;
+
+
+   sortedByFriends = _.sortBy(dataJson, function(obj) {
+      return obj.friends.length;
+   });
+
+   let namesArray = [];
+
+   // console.log('Objects sorted by friends', sortedByFriends);
+
+   _.forEach(sortedByFriends, function(obj) {
+      namesArray += obj.name;
+
+      namesArray += ',';
+   });
+
+   namesArray = namesArray.split(',');
+   namesArray = _.compact(namesArray);
+
+   console.log('Names array:', namesArray);
+
+   /*          Friends        */
+
+   let friendsArray = [];
+
+   _.forEach(dataJson, function(obj) {
+      _.forEach(obj.friends, function(item) {
+         friendsArray += item.name;
+
+         friendsArray += ',';
+      })
+   });
+
+   friendsArray = friendsArray.split(',');
+   friendsArray = _.compact(friendsArray);
+   friendsArray = _.uniq(friendsArray);
+
+   console.log('Friends array:', friendsArray);
+
 });
